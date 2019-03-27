14e364ab6cf7138557a15737ef6d20d0b989dfa6
coslund <38672497+coslund@users.noreply.github.com>
2019-03-26 22:30:58 -0700

    update the title

--- game.js
+++ game.js
@@ -39,7 +39,8 @@ var startGame = function() {
     Game.setBoard(1,new Starfield(50,0.6,100));
     Game.setBoard(2,new Starfield(100,1.0,50));
   }  
-  Game.setBoard(3,new TitleScreen("Alien Invasion", 
+  Game.setBoard(3,new TitleScreen("Alien Invasion",
+                                  "Demo for WGU",
                                   "Press fire to start playing",
                                   playGame));
 };
